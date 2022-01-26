6e9cc770f4ade99eb1d1c9d5fb2c87a742f068ae
vinay-keshava <its.vicky30@gmail.com>
2022-01-26 11:06:29 +0530

    rm key.txt

--- department.php
+++ department.php
@@ -1,34 +1,23 @@
 <?php
-echo "Vinay"
+		$conn = mysqli_connect("localhost", "root", "", "medstar1");
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+	echo "hi";
+        $dpt_id=strval($_REQUEST['dpt_id']);
+        $dpt_name=strval($_REQUEST['dpt_name']);
+	$dpt_location=strval($_REQUEST['$dept_location']);
 
-        // servername => localhost
-        // username => root
-        // password => empty
-        // database name => staff
-        $conn = mysqli_connect("localhost", "root", "", "medstar1");
-        // Check connection
-        if($conn === false){
-            die("ERROR: Could not connect. "
-                . mysqli_connect_error());
-        }
+//	echo $staff_id;
+		$sql = "INSERT INTO department (dpt_id,dpt_name,dpt_location) VALUES ('$dpt_id','$dpt_name','$dpt_location')";
+	echo $sql;
+		if(mysqli_query($conn, $sql)){
+//		echo '<a href="'.$link_address.'">Link</a>';
+		echo "successfull";
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
 
- 
-        $dpt_id =$_REQUEST['dpt_id'];
-        $dpt_name =$_REQUEST['dpt_name'];
-        $dpt_location =  $_REQUEST['dpt_location'];
-
- // Performing insert query execution
-        // here our table name is department
-        $sql = "INSERT INTO department(dpt_id,dpt_name,dpt_location) VALUES('$dpt_id','$dpt_name','$dpt_location')";
-
-        if(mysqli_query($conn, $sql)){
-
-echo  "Successfull";
-
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($conn);
-        }
-        // Close connection
-//        mysqli_close($conn);
-        ?>
+		}
+?>
